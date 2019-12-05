e9f9f170ccbe0f8beba37cb794cbf9084afcf598
taterntots <matthew.a.weidner@gmail.com>
2019-12-05 15:16:35 -0500

    set up some basic functionality in App for the counter. Currently made a rudimentary counter function that adds one count per button click.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({
+    ballCount: 0,
+    strikeCount: 0,
+    hitCount: 0,
+    foulCount: 0
+  })
+
+  const updateCount = (name, count) => {
+    count = count + 1;
+    setData({...data, [name]: count});
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Web Application Testing</h1>
+        <h1>BaseBall Counter</h1>
+        <h2>{data.ballCount}</h2>
+        <button onClick={() => updateCount('ballCount', data.ballCount)}>Ball Count</button>
       </header>
     </div>
   );
