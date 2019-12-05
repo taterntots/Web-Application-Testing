3edf911f2d52bf443adadf22c0d79f29a2721214
taterntots <matthew.a.weidner@gmail.com>
2019-12-05 17:50:45 -0500

    cleaned up some unused code. Also attempted to add tests for various functions, but was unsuccessful.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,22 +4,6 @@ import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
-  // const [data, setData] = useState({
-  //   ballCount: 0,
-  //   strikeCount: 0,
-  // })
-
-  // const updateCount = (name, count) => {
-  //   if (name === 'strikeCount' && count === 3) {
-  //     count = 0;
-  //   } else if (name === 'ballCount' && count === 4) {
-  //     count = 0;
-  //   } else {
-  //     count = count + 1;
-  //   }
-  //   setData({...data, [name]: count});
-  // }
-
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
 
@@ -58,15 +42,46 @@ function App() {
         <h1>BaseBall Counter</h1>
         <Display balls={balls} strikes={strikes} />
         <Dashboard ball={ball} strike={strike} foul={foul} hit={hit} />
-        {/* <h2>{data.ballCount}</h2>
-        <button onClick={() => updateCount('ballCount', data.ballCount)}>Ball Count</button>
-        <h2>{data.strikeCount}</h2>
-        <button onClick={() => updateCount('strikeCount', data.strikeCount)}>Strike Count</button>
-        <h2>{data.foulCount}</h2>
-        <button onClick={() => foulBall()}>Foul Ball</button> */}
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+//------------------------------------------------OLD CODE----------------------------------------------------//
+
+
+// function App() {
+//   const [data, setData] = useState({
+//     ballCount: 0,
+//     strikeCount: 0,
+//   })
+
+//   const updateCount = (name, count) => {
+//     if (name === 'strikeCount' && count === 3) {
+//       count = 0;
+//     } else if (name === 'ballCount' && count === 4) {
+//       count = 0;
+//     } else {
+//       count = count + 1;
+//     }
+//     setData({...data, [name]: count});
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h2>{data.ballCount}</h2>
+//         <button onClick={() => updateCount('ballCount', data.ballCount)}>Ball Count</button>
+//         <h2>{data.strikeCount}</h2>
+//         <button onClick={() => updateCount('strikeCount', data.strikeCount)}>Strike Count</button>
+//         <h2>{data.foulCount}</h2>
+//         <button onClick={() => foulBall()}>Foul Ball</button>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, { ball, hit } from './App';
+import expectExport from 'expect';
 
 test('renders my app without crashing', () => {
   render(<App />);
@@ -17,3 +18,25 @@ test('the dashboard shows the correct buttons', () => {
   getByText(/foul ball!/i)
   getByText(/hit!/i)
 })
+
+// test('hit resets both balls and strikes counter', () => {
+//   expect(hit()).toStrictEqual({balls: 0, strikes: 0});
+// })
+
+// const hit = () => {
+//   setStrikes(0);
+//   setBalls(0);
+// }
+
+// test('the fourth ball resets the ball count to zero', () => {
+//   expect(ball(3)).toBe(0);
+// })
+
+// const ball = () => {
+//   if (balls < 3) {
+//     setBalls(balls + 1);
+//   } else if (balls === 3) {
+//     setBalls(0);
+//     setStrikes(0);
+//   }
+// }
